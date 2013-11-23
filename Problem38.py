b9df2fa0261bb1bdcb3e8095c2f86129e9672c79
Ben Roux <liquid.ise@gmail.com>
2013-11-23 00:52:36 -0700

    38 solution added

--- Problem38.py
+++ Problem38.py
@@ -1,3 +1,27 @@
 from number_theory import isPandigital
 
-max_pandigital = 987654321
+finds = []
+for i in range( 2, 9999 ):
+	found = True
+	digits = list( str(i) )
+	set_digits = set( digits )
+
+	for multiplier in range( 2, 10 ):
+		new_product = i * multiplier
+		new_product = list( str(new_product) )
+		digits += new_product
+		set_digits = set_digits | set( new_product )
+
+		if '0' in set_digits or len( set_digits ) != len( digits ):
+			found = False
+			break
+		elif len( digits ) == 9:
+			found = True
+			break
+
+	if found:
+		finds.append( "".join( digits ) )
+
+finds.sort()
+
+print finds[ -1 ]
