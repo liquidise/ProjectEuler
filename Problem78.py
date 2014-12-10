c34fe1bd51450fee371ed9c5d062ae3522e4d4b7
Ben Roux <liquid.ise@gmail.com>
2014-12-10 14:04:33 -0700

    Adding merge

--- Problem78.py
+++ Problem78.py
@@ -18,12 +18,13 @@ def num_ways( number, highest_number ):
 	cache[ key ] = ways
 	return ways
 
-numbers = range( 1, 1000 )
+MAX = 1000
+
+numbers = range( 1, MAX + 1 )
 numbers.reverse()
 
-for i in range( 1, 1000 ):
+for i in range( 1, MAX ):
 	piles = num_ways( i, numbers[0] )
-	print i, piles
-	if piles % 1000000 == 0:
+	# print i, piles
+	if piles % 100 == 0:
 		print "FOUND", i, piles
-		exit()
